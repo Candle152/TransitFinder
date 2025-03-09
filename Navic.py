99c65d9e070fadb9b5c2622e0c5e6e119e6e341b
zhudi <gcyjo165@163.com>
2025-03-09 13:31:30 +0800

    add ApiCline to contral the url and params

--- Navic.py
+++ Navic.py
@@ -1,46 +1,56 @@
 from tools import JKit
 import sys
 
-key = ''
 
+class ApiClient:
+    def __init__(self):
+        self.url = "https://restapi.amap.com/v3/bus/linename?parameters"
+        self.params = {
+            'key': '',
+            'city': '',
+            'keywords': '',
+            'extensions': 'base'
+        }
 
-def getLine(linename, city):
-    """
-    get the line for the bus in the city
-    return JSON
-    """
-    global key
+    def setCity(self, city):
+        self.params['city'] = city
 
-    url = "https://restapi.amap.com/v3/bus/linename?parameters"
-    params = {
-        'key': key,
-        'city': city,
-        'keywords': linename,
-        'extensions': 'all'
-    }
-    data = JKit.load_url(url, params)
-    try:
-        if data['count'] != 0:
-            return data['buslines'][0]
-        else:
-            return dict()
-    except Exception:
-        print(data['info'])
-        if data['info'] == 'USER_DAILY_QUERY_OVER_LIMIT':
-            print('The key is depleted')
+    def setKey(self, key):
+        self.params['key'] = key
+
+    def setKeywords(self, keywords):
+        self.params['keywords'] = keywords
+
+    def setExtensions(self, all):
+        if all:
+            self.params['extensions'] = 'all'
+
+    def getLine(self):
+        """
+        get the line for the bus in the city
+        return JSON
+        """
+        data = JKit.load_url(self.url, self.params)
+        try:
+            if data['count'] != 0:
+                return data['buslines']
+            else:
+                return dict()
+        except Exception:
+            print(data['info'])
+            if data['info'] == 'USER_DAILY_QUERY_OVER_LIMIT':
+                print('The key is depleted')
 
 
 def main():
     userInput = sys.argv[1:]
-    global key
-    key = userInput[2]
-    line = getLine(userInput[0], userInput[1])
-    print('id:\t' + line['id'])
-    print('type:\t' + line['type'])
-    print('name:\t' + line['name'])
-    print('basic_price\t' + line['basic_price'])
-    print('stops:')
-    print(line['busstops'])
+    apc = ApiClient()
+    apc.setKey(userInput[0])
+    apc.setCity(userInput[1])
+    apc.setKeywords(userInput[2])
+    lines = apc.getLine()
+    for line in lines:
+        print(line)
 
 
 if __name__ == '__main__':
