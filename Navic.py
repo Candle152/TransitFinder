990db397b640a466a37c08c75cce04856f88083d
zhudi <gcyjo165@163.com>
2025-03-09 19:51:55 +0800

    delete the required and add the check function

--- Navic.py
+++ Navic.py
@@ -52,18 +52,25 @@ def readCommand(argv):
     parser = argparse.ArgumentParser(description="基于高德api的公交查询工具")
     parser.add_argument('-k', '--key', type=str, help='设置密钥')
     parser.add_argument('-c', '--city', type=str,
-                        required=True, help='设置查询城市(中文,全拼音,城市编码)')
+                        help='设置查询城市(中文,全拼音,城市编码)')
     parser.add_argument('-l', '--line', type=str,
-                        required=True, help='设置想要查询线路')
+                        help='设置想要查询线路')
     parser.add_argument('-a', '--all',
                         action='store_true', help='返回详细信息')
-    parser.add_argument('-n', '--number', type=int, help='设置最大查询个数')
+    parser.add_argument('-n', '--number', type=int, help='设置最大查询个数，默认1')
+    parser.add_argument('--set-key', type=str, help='设置默认key')
 
     # Parsing parameters and return
     return parser.parse_args()
 
 
 def processCommand(args, apc):
+    if not args.city:
+        print("error: the following arguments are required: -c/--city")
+        exit(1)
+    if not args.line:
+        print("error: the following arguments are required: -l/--line")
+        exit(1)
     if args.key:
         apc.setKey(args.key)
     if args.number:
